4cf067578170fa86ebcabf4fd9cd79715911f397
Anton Piccardo-Selg <anton.piccardo.selg@gmail.com>
2017-06-01 20:48:21 +0100

    Add sse with channels

--- web/boot.sh
+++ web/boot.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-# TODO check if it makes sense to increase processes in a container.
-# uwsgi --http 0.0.0.0:8000 --module wsgi:application  --gevent 100
 gunicorn wsgi:application --worker-class gevent --bind 0.0.0.0:8000
