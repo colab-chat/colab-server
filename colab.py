1b4b7359456b66606c79d0de9edb76ae0154f6ea
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2017-06-02 13:22:56 +0100

    Add python script to run environment

---
+++ colab.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python 
+
+import argparse
+from subprocess import call
+
+parser = argparse.ArgumentParser(description='CoLab Startup')
+
+# Add acceptable arguments
+parser.add_argument('-r', '--run',type=int, choices=[0, 1, 2], help='set run type: 0=production, 1=development, 2=testing', default=0)
+parser.add_argument('-d', '--detach', help='start docker in detached mode')
+
+
+# Get the arguments
+args = parser.parse_args()
+
+# -------------------------------------------
+# Act on the arguments
+# ------------------------------------------
+
+# Check if we should run in detached mode
+
+
+
+# Set up sub system call
+run_type = args.run
+if run_type == 1:
+  print("Running in development mode")
+  call(["docker-compose", "-f", "docker-compose.yaml", "-f", "docker-compose.development.yaml", "up"])
+elif run_type == 2:
+  print("Running in testing mode")
+else:
+  print("Running in production mode")
+  call(["docker-compose", "-f", "docker-compose.yaml", "-f", "docker-compose.production.yaml", "up"])
+
