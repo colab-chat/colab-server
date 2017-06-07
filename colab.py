261a6ff8e89c6bdb217748c9f179e7d2efe9bd09
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2017-06-07 13:19:40 +0100

    Add flask debug toolbar

--- colab.py
+++ colab.py
@@ -9,44 +9,57 @@ parser = argparse.ArgumentParser(description='CoLab Startup')
 parser.add_argument('-r', '--run', type=int, choices=[0, 1, 2],
                     help='set run type: 0=production, 1=development, 2=testing', default=0)
 parser.add_argument('-d', '--detach', action='store_true', help='start docker in detached mode')
+parser.add_argument('-s', '--stop', action='store_true', help='stop docker containers')
+parser.add_argument('-b', '--build', action='store_true', help='build docker containers')
 args = parser.parse_args()
 
 
-# ---------------------------------
-# Set up process for docker compose
-# ---------------------------------
-process_list = ["docker-compose", "-f", "docker-compose.yaml"]
 
+# -------------------------------
+# Check if colab is to be stopped
+# ------------------------------
+if args.stop:
+  # TODO need to check if other commands have been set
+  process_list = ["docker-compose", "stop"]
+elif args.build:
+  # TODO need to check if other commands have been set
+  process_list = ["docker-compose", "build"]
+else:
+  # ---------------------------------
+  # Set up process for docker compose
+  # ---------------------------------
+  process_list = ["docker-compose", "-f", "docker-compose.yaml"]
+ 
 
-# -----------------------------
-# Select correct docker-compose
-# -----------------------------
-run_type = args.run
-if run_type == 1:
+  # -----------------------------
+  # Select correct docker-compose
+  # -----------------------------
+  run_type = args.run
+  if run_type == 1:
     print("Running in development mode")
     development_flags = ["-f",  "docker-compose.development.yaml"]
     process_list.extend(development_flags)
-elif run_type == 2:
+  elif run_type == 2:
     print("Running in testing mode")
-else:
+  else:
     print("Running in production mode")
     production_flags = ["-f",  "docker-compose.production.yaml"]
     process_list.extend(production_flags)
 
-# ---------
-# Up docker
-# ---------
-process_list.append("up")
+  # ---------
+  # Up docker
+  # ---------
+  process_list.append("up")
 
-# ---------------------------------------
-# Add detached mode
-# ---------------------------------------
-detach_mode = args.detach
-if detach_mode:
+  # ---------------------------------------
+  # Add detached mode
+  # ---------------------------------------
+  detach_mode = args.detach
+  if detach_mode:
     process_list.append("-d")
 
 # ----------------
 # Run docker
 # ----------------
-print
 call(process_list)
+
--- web/colab_server/__init__.py
+++ web/colab_server/__init__.py
@@ -3,6 +3,8 @@ from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_sse import sse
+from flask_debugtoolbar import DebugToolbarExtension
+
 
 # ----------------
 # Flask extensions
@@ -10,6 +12,7 @@ from flask_sse import sse
 db = SQLAlchemy()
 bootstrap = Bootstrap()
 mail = Mail()
+debug_toolbar = DebugToolbarExtension()
 
 
 def create_app(configuration):
@@ -28,6 +31,7 @@ def create_app(configuration):
     db.init_app(app)
     mail.init_app(app)
     bootstrap.init_app(app)
+    debug_toolbar.init_app(app)
 
     # --------------------
     # Authentication setup
