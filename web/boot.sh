bb246510de28acebfcc607c0a66db6cc2538d667
Matthew D Jones <matthew.d.jones@tessella.com>
2017-06-26 19:37:47 +0100

    re #19 remove unnecessary sleep from web app launch script

--- web/boot.sh
+++ web/boot.sh
@@ -4,9 +4,8 @@
 kafkacat -b kafka -L
 OUT=$?
 i="0"
-while [ $OUT -ne 0 -a  $i != 10  ]; do
+while [ $OUT -ne 0 -a  $i -ne 10  ]; do
    echo "Waiting for Kafka to be ready"
-   sleep 1
    kafkacat -b kafka -L
    OUT=$?
    i=$[$i+1]
