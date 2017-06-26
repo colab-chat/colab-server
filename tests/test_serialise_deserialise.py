917251477f29c8612afe60a0ad07664fc6feb5a9
Matthew D Jones <matthew.d.jones@tessella.com>
2017-06-26 20:02:08 +0100

    re #18 add a failing test which pytest discovers

---
+++ tests/test_serialise_deserialise.py
@@ -0,0 +1,8 @@
+import env
+import pytest
+from web.colab_server.streaming.avroserialiser import AvroSerialiser
+from web.colab_server.streaming.avrodeserialiser import AvroDeserialiser
+
+
+def test_fails():
+    assert False
