aa14e9eeafbee6eae58182b19628c53d3721908c
Matthew D Jones <matthew.d.jones@tessella.com>
2017-06-26 19:26:41 +0100

    re #19 use kafkacat to check Kafka is ready before starting web app

--- web/boot.sh
+++ web/boot.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-# Wait 15 seconds to make sure Kafka is ready before starting
-sleep 15
+# Wait for Kafka to be ready before starting the web app
+kafkacat -b kafka -L
+OUT=$?
+i="0"
+while [ $OUT -ne 0 -a  $i != 10  ]; do
+   echo "Waiting for Kafka to be ready"
+   sleep 1
+   kafkacat -b kafka -L
+   OUT=$?
+   i=$[$i+1]
+done
+
 gunicorn wsgi:application --worker-class gevent --bind 0.0.0.0:8000
