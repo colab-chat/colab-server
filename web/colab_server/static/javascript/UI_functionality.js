bd40994e30a9e650fb5976170445642864f5832c
Matthew D Jones <matthew.d.jones@tessella.com>
2017-08-06 16:57:53 +0100

    add js and css files for temporary front end

---
+++ web/colab_server/static/javascript/UI_functionality.js
@@ -0,0 +1,112 @@
+var MyProject = {};
+
+    $(document).ready(function(){
+        $('.user-profile').click(function() {
+            $('.chat-container').hide();
+            $('.user-profile.active').removeClass('active');
+
+            $('#'+$(this).attr('data-up')).show();
+            $(this).addClass('active');
+        });
+
+        $('.script-editor').click(function() {
+            $('.editor-input').hide();
+            $('.script-editor.active').removeClass('active');
+
+            $('#'+$(this).attr('data-up')).show();
+            $(this).addClass('active');
+        });
+
+        var cmeditor1 = CodeMirror.fromTextArea(document.getElementById("editor1-textarea"), {
+            mode: {name: "stex",
+                   version: 3,
+                   singleLineStringErrors: false},
+            lineNumbers: false,
+            indentUnit: 4,
+            matchBrackets: true
+        });
+
+        var cmeditor2 = CodeMirror.fromTextArea(document.getElementById("editor2-textarea"), {
+            mode: {name: "python",
+                   version: 3,
+                   singleLineStringErrors: false},
+            lineNumbers: true,
+            indentUnit: 4,
+            matchBrackets: true
+        });
+
+        var cmeditor3 = CodeMirror.fromTextArea(document.getElementById("editor3-textarea"), {
+            mode: {name: "r",
+                   version: 3,
+                   singleLineStringErrors: false},
+            lineNumbers: true,
+            indentUnit: 4,
+            matchBrackets: true
+        });
+
+        $('.chat-container').hide();
+        $('#General').show();
+        $('.editor-input').hide();
+        $('#editor1').show();
+
+        cmeditor1.on('focus',function(){
+            if( cmeditor1.getValue() == "Type text here" ) {
+                cmeditor1.setValue("");
+            }
+        });
+
+        cmeditor2.on('focus',function(){
+            if( cmeditor2.getValue() == "Type Python code here" ) {
+                cmeditor2.setValue("");
+            }
+        });
+
+        cmeditor3.on('focus',function(){
+            if( cmeditor3.getValue() == "Type R code here" ) {
+                cmeditor3.setValue("");
+            }
+        });
+
+        MyProject.cmeditor1 = cmeditor1;
+        MyProject.cmeditor2 = cmeditor2;
+        MyProject.cmeditor3 = cmeditor3;
+
+        console.log(cmeditor1.getValue());
+        console.log(MyProject.cmeditor1.getValue());
+
+    });
+
+
+    function showUI(ele){
+        console.log($(ele));
+        var kids = $(ele).children(), temp;
+        for( var i = kids.length-1 ; i >=0  ; i-- ){
+            temp  = $(kids[i]);
+
+            if(temp.is('div')){
+                temp.animate({
+                    marginTop:0,
+                },400).css({opacity:1}).fadeIn()
+            }
+            else{
+                temp.css({opacity:1}).fadeIn()
+            }
+        }
+    }
+
+    function hideUI(ele){
+        console.log($(ele));
+        var kids = $(ele).children(), temp;
+        for( var i = kids.length-1 ; i >=0  ; i-- ){
+            temp  = $(kids[i]);
+
+            if(temp.is('div')){
+                temp.animate({
+                    marginTop:'30px',
+                }).css({opacity:0});
+            }
+            else{
+                temp.css({opacity:0});
+            }
+        }
+    }
