c42abec80f0acc7c06986992d0c0497e339e0d71
Matthew D Jones <matthew.d.jones@tessella.com>
2017-08-06 17:21:38 +0100

    add a send route

--- web/colab_server/main_view.py
+++ web/colab_server/main_view.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, current_app, request
 from flask_user import login_required
 from .flask_sse_kafka import sse
 from .messages.message_text import TextMessage
@@ -23,4 +23,15 @@ def publish_hello():
     sse.publish(
         TextMessage('author', 'last_author', datetime.now(), datetime.now(),
                     'test_avro_topic', 'Life is good!'))
+    return "Message sent!"
+
+
+@main.route('/send', methods=['POST'])
+def send():
+    if request.form['type'] == 'text':
+        sse.publish(
+            TextMessage('author', 'last_author', datetime.now(), datetime.now(),
+                        request.form['topic'], request.form['message']))
+    else:
+        raise ValueError('Unrecognised message type in views.py send()')
     return "Message sent!"
