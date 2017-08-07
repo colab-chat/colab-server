eef495279cee90927648aa3aa711c7f2197e34d9
Matthew D Jones <matthew.d.jones@tessella.com>
2017-08-07 07:32:39 +0100

    remove superfluous os.path.dirname in test env script

--- tests/env.py
+++ tests/env.py
@@ -4,8 +4,6 @@ import os
 # append module root directory to sys.path
 sys.path.append(
     os.path.dirname(
-        os.path.dirname(
-            os.path.abspath(__file__)
-        )
+        os.path.abspath(__file__)
     )
 )
